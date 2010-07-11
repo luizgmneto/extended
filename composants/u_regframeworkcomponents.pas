25455069746fdcafe9deab6ddcdedf1ccf3c5903
girouxmatthieu <girouxmatthieu@localhost>
2010-07-11 17:26:47 +0200

    Regrouping components

--- composants/u_regframeworkcomponents.pas
+++ composants/u_regframeworkcomponents.pas
@@ -8,27 +8,39 @@ unit u_regframeworkcomponents;
 interface
 
 uses
-  Classes, SysUtils,
+  Classes,
 {$IFDEF FPC}
   lresources,
 {$ENDIF}
-  u_framework_components;
+  SysUtils;
 
 
 procedure Register;
 
 implementation
 
-uses unite_messages;
+uses unite_messages, U_DBListView, U_ExtDBNavigator,u_framework_components,
+     U_OnFormInfoIni,U_ExtNumEdits,U_ExtColorCombos,U_ExtDBImage,
+     ComponentEditors, dbpropedits, PropEdits;
 
 procedure Register;
 begin
-  RegisterComponents(CST_PALETTE_COMPOSANTS, [TFWDBEdit,TFWDBLookupCombo,TFWDBGrid,TFWLabel,TFWDBMemo,TFWDBDateEdit{$IFNDEF FPC},TFWDBDateTimePicker{$ENDIF}]);
+  RegisterComponents(CST_PALETTE_COMPOSANTS, [TDBListView,TOnFormInfoIni,TExtDBNavigator,TExtNumEdit,TExtDBImage,TExtDBNumEdit,TExtColorCombo,TExtDBColorCombo,TFWDBEdit,TFWDBLookupCombo,TFWDBGrid,TFWLabel,TFWDBMemo,TFWDBDateEdit{$IFNDEF FPC},TFWDBDateTimePicker{$ENDIF}]);
+{$IFDEF DELPHI}
+  RegisterPropertyEditor ( TypeInfo(string), TExtDBNavigator, 'SortField', TDataFieldProperty);
+{$ENDIF}
+  RegisterPropertyEditor ( TypeInfo({$IFDEF FPC}ShortString {$ELSE}string{$ENDIF}), TDBListView, 'DataKeyUnit'   , {$IFDEF FPC}TFieldProperty{$ELSE}TDataFieldProperty{$ENDIF});
+  RegisterPropertyEditor ( TypeInfo({$IFDEF FPC}ShortString {$ELSE}string{$ENDIF}), TDBListView, 'DataSort'      , {$IFDEF FPC}TFieldProperty{$ELSE}TDataFieldProperty{$ENDIF});
+  RegisterPropertyEditor ( TypeInfo(string), TDBListView, 'DataFieldsDisplay'   , {$IFDEF FPC}TFieldProperty{$ELSE}TDataFieldProperty{$ENDIF});
 end;
 
 initialization
 {$IFDEF FPC}
   {$i u_framework_components.lrs}
+  {$I U_RegImageComponents.lrs}
+  {$i U_ExtDBNavigator.lrs}
+  {$i U_DBListView.lrs}
+  {$i U_OnFormInfoIni.lrs}
 {$ENDIF}
 end.
 
