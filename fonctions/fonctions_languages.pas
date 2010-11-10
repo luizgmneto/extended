ce4283172fd304beca1faacc2b550e3b415cd465
girouxmatthieu <girouxmatthieu@localhost>
2010-11-10 15:11:08 +0100

    new unit

---
+++ fonctions/fonctions_languages.pas
@@ -0,0 +1,20 @@
+unit fonctions_languages;
+
+{$IFDEF FPC}
+{$mode Delphi}
+{$ENDIF}
+
+
+interface
+
+resourcestring
+ GS_LOGIN    = 'Login';
+ GS_PASSWORD = 'Password';
+
+uses
+  Classes, SysUtils; 
+
+implementation
+
+end.
+
