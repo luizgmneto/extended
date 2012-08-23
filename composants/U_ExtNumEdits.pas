e904b9794748d5dfde05785b3348b799403fbaf4
girouxmatthieu <girouxmatthieu@localhost>
2012-08-23 11:01:42 +0200

    optimising

--- composants/U_ExtNumEdits.pas
+++ composants/U_ExtNumEdits.pas
@@ -47,24 +47,26 @@ uses
                                                FileUnit : 'U_NumEdits' ;
                                                Owner : 'Matthieu Giroux' ;
                                                Comment : 'Edition de nombres.' ;
-                                               BugsStory : '1.0.1.2 : Mask on num edit' + #13#10
+                                               BugsStory : '1.0.1.3 : Testing on LAZARUS' + #13#10
+                                                         + '1.0.1.2 : Mask on num edit' + #13#10
                                                          + '1.0.1.1 : NumRounded property not tested' + #13#10
                                                          + '1.0.1.0 : Better ExtNumEdit with good colors' + #13#10
                                                          + '1.0.0.1 : Bug rafraîchissement de AValue' + #13#10
                                                          + '1.0.0.0 : Gestion en place.';
                                                UnitType : 3 ;
-                                               Major : 1 ; Minor : 0 ; Release : 1 ; Build : 1 );
+                                               Major : 1 ; Minor : 0 ; Release : 1 ; Build : 3 );
     gVer_TExtDBNumEdit : T_Version = ( Component : 'Composant TExtDBNumEdit' ;
                                                FileUnit : 'U_NumEdits' ;
                                                Owner : 'Matthieu Giroux' ;
                                                Comment : 'Edition de nombres en données.' ;
-                                               BugsStory : '1.0.1.3 : Better num edit' + #13#10
+                                               BugsStory : '1.0.1.4 : Testing on LAZARUS' + #13#10
+                                                         + '1.0.1.3 : Better num edit' + #13#10
                                                          + '1.0.1.2 : NumRounded property not tested' + #13#10
                                                          + '1.0.1.1 : Less methods with good colors' + #13#10
                                                          + '1.0.1.0 : Améliorations sur la gestion des erreurs' + #13#10
                                                          + '1.0.0.0 : Gestion en place.';
                                                UnitType : 3 ;
-                                               Major : 1 ; Minor : 0 ; Release : 1 ; Build : 3 );
+                                               Major : 1 ; Minor : 0 ; Release : 1 ; Build : 4 );
 {$ENDIF}
     CST_MC_NEGATIVE = True ;
     CST_MC_POSITIVE = True ;
@@ -363,7 +365,7 @@ begin
     if FDataLink.Field.IsNull then
       gre_AValue := 0
     Else
-      gre_AValue := FDataLink.Field.AsFloat ;
+      gre_AValue := FDataLink.Field.Value ;
   FDataLink.Modified;
   inherited Change;
 end;
