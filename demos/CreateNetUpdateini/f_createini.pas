2af61f8986c7377f8151d3c03eb254d8ed17bef3
girouxmatthieu <girouxmatthieu@localhost>
2013-10-09 20:25:15 +0200

    update project

---
+++ demos/CreateNetUpdateini/f_createini.pas
@@ -0,0 +1,361 @@
+unit f_createini;
+
+{$IFDEF FPC}
+{$MODE Delphi}
+{$R *.lfm}
+{$ENDIF}
+{ crée ceci :
+[UPDATE]
+Nom=Ancestromania.exe
+Version=2013.0
+VersionExe=2013.0.0.671
+Taille=3934208
+Date=08/02/2013 17:17:34
+TailleMAJC=10230864
+VersionBase=5.159
+md5=8b1d3a9bc30a57624c338a395f507ba6
+}
+
+interface
+
+uses
+  SysUtils, Classes, Graphics, Controls, Forms, fonctions_system, Dialogs,
+  StdCtrls, FileCtrl, ExtCtrls, LResources, EditBtn, UniqueInstance,
+  U_OnFormInfoIni, IBDatabase, IBSQL;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Creer: TButton;
+    See: TButton;
+    CreerCarboni386: TButton;
+    CreerCarbon64: TButton;
+    CreerCarbonARM: TButton;
+    CreerCarbonPPC: TButton;
+    CreerDebianGnomei386: TButton;
+    CreerDebianGnomeARM: TButton;
+    CreerDebianGnomei64: TButton;
+    CreerDebianGnomePPC: TButton;
+    CreerDebianKDE386: TButton;
+    CreerDebianKDEARM: TButton;
+    CreerDebianKDEi64: TButton;
+    CreerDebianKDEPPC: TButton;
+    CreerRedhatGnomei386: TButton;
+    CreerRedhatKDEARM1: TButton;
+    CreerTarGnomei386: TButton;
+    CreerRedhatGnomeARM: TButton;
+    CreerTarGnomeARM: TButton;
+    CreerRedhatGnomei64: TButton;
+    CreerTarGnomei64: TButton;
+    CreerRedhatGnomePPC: TButton;
+    CreerTarGnomePPC: TButton;
+    CreerRedhatKDEi386: TButton;
+    CreerTarKDEARM: TButton;
+    CreerRedhatKDEi64: TButton;
+    CreerTarKDEi64: TButton;
+    CreerRedhatKDEPPC: TButton;
+    CreerTarKDEi386: TButton;
+    CreerTarKDEPPC: TButton;
+    CreerWini386: TButton;
+    CreerWini64: TButton;
+    DirectoryDest: TDirectoryEdit;
+    DirectoryExe: TDirectoryEdit;
+    DirectoryListBox: TDirectoryEdit;
+    FileWin32: TFileNameEdit;
+    FileWin33: TFileNameEdit;
+    FileWin34: TFileNameEdit;
+    FileWin35: TFileNameEdit;
+    FileWin36: TFileNameEdit;
+    FileWin37: TFileNameEdit;
+    FileWin38: TFileNameEdit;
+    FileWin39: TFileNameEdit;
+    FileWin40: TFileNameEdit;
+    FileWin41: TFileNameEdit;
+    FileWin42: TFileNameEdit;
+    FileWin43: TFileNameEdit;
+    FileWin44: TFileNameEdit;
+    FileWin45: TFileNameEdit;
+    FileWin46: TFileNameEdit;
+    FileWin47: TFileNameEdit;
+    FileWin48: TFileNameEdit;
+    FileWin49: TFileNameEdit;
+    FileWin50: TFileNameEdit;
+    FileWin51: TFileNameEdit;
+    FileWin52: TFileNameEdit;
+    FileWin53: TFileNameEdit;
+    FileWin54: TFileNameEdit;
+    FileWin55: TFileNameEdit;
+    FileWin56: TFileNameEdit;
+    FileWin57: TFileNameEdit;
+    FileWin58: TFileNameEdit;
+    FileWin59: TFileNameEdit;
+    FileWin60: TFileNameEdit;
+    FileWin61: TFileNameEdit;
+    FileBase: TFileNameEdit;
+    FilterComboBox: TFilterComboBox;
+    ibd_BASE: TIBDatabase;
+    IBT_BASE: TIBTransaction;
+    Label1: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    Label16: TLabel;
+    Label17: TLabel;
+    Label18: TLabel;
+    Label19: TLabel;
+    Label2: TLabel;
+    Label20: TLabel;
+    Label21: TLabel;
+    Label22: TLabel;
+    Label23: TLabel;
+    Label24: TLabel;
+    Label25: TLabel;
+    Label26: TLabel;
+    Label27: TLabel;
+    Label28: TLabel;
+    Label29: TLabel;
+    Label3: TLabel;
+    Label30: TLabel;
+    Label31: TLabel;
+    Label32: TLabel;
+    Label33: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    OnFormInfoIni: TOnFormInfoIni;
+    PanelBottom: TPanel;
+    Panel10: TPanel;
+    Panel11: TPanel;
+    Panel12: TPanel;
+    Panel13: TPanel;
+    Panel14: TPanel;
+    Panel15: TPanel;
+    Panel16: TPanel;
+    Panel17: TPanel;
+    Panel18: TPanel;
+    Panel19: TPanel;
+    Panel2: TPanel;
+    Panel20: TPanel;
+    Panel21: TPanel;
+    Panel22: TPanel;
+    Panel23: TPanel;
+    Panel24: TPanel;
+    Panel25: TPanel;
+    Panel26: TPanel;
+    Panel27: TPanel;
+    Panel28: TPanel;
+    Panel29: TPanel;
+    Panel3: TPanel;
+    Panel30: TPanel;
+    Panel31: TPanel;
+    Panel32: TPanel;
+    Panel33: TPanel;
+    Panel4: TPanel;
+    Panel5: TPanel;
+    Panel6: TPanel;
+    Panel7: TPanel;
+    Panel8: TPanel;
+    Panel9: TPanel;
+    Q: TIBSQL;
+    UniqueInstance: TUniqueInstance;
+    procedure CreerClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure SeeClick(Sender: TObject);
+  private
+    function CreeContext:Boolean;
+    procedure CreerIniFromObject(const Sender: TObject);
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+    procedure AutoCreeIni ( const as_FileName : String ; const as_Name : String );
+    procedure CreeIni ( const as_FileName : String ; const aPackage : TPackageType; const aar_Architecture : TArchitectureType ; const aProcType : TProcessorType );
+  public
+    { Déclarations publiées }
+    procedure CreerUnIni(Sender: TObject);
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+
+uses LConvEncoding,
+     u_common_functions,
+     FileUtil,
+     IniFiles,
+     IBIntf,
+     u_connexion,
+     u_genealogy_context,
+     u_common_const, md5;
+
+{ TForm1 }
+
+procedure TForm1.CreerClick(Sender: TObject);
+var i : Integer;
+    lcom_Component : TComponent;
+begin
+ if CreeContext Then
+ for i := 0 to ComponentCount - 1 do
+   Begin
+    lcom_Component := Components[i];
+    if  ( lcom_Component is TButton )
+    and (( lcom_Component as TControl ).Parent <> PanelBottom )
+     Then  CreerIniFromObject( lcom_Component );
+   end;
+end;
+
+
+procedure TForm1.CreerUnIni(Sender: TObject);
+var i : Integer;
+    lcon_Control : TControl;
+Begin
+ if CreeContext Then
+   CreerIniFromObject ( Sender );
+end;
+
+procedure TForm1.CreerIniFromObject(const Sender: TObject);
+var i : Integer;
+    lcon_Control : TControl;
+Begin
+ with ( Sender as TControl ) do
+ for i := 0 to Parent.ControlCount - 1 do
+   Begin
+    lcon_Control := Parent.Controls[i];
+    if   ( lcon_Control is TFileNameEdit )
+    and (( lcon_Control as TFileNameEdit ).FileName > '' )
+     Then
+       AutoCreeIni (( lcon_Control as TFileNameEdit ).FileName, Name );
+   end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var i : Integer;
+    lcom_Component : TComponent;
+begin
+  for i := 0 to ComponentCount - 1 do
+   Begin
+    lcom_Component := Components[i];
+    if  ( lcom_Component is TButton )
+    and not assigned (( lcom_Component as TButton ).OnClick )
+     Then ( lcom_Component as TButton ).OnClick := TNotifyEvent ( CreerUnIni );
+   end;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  try
+    if IBT_BASE.Active Then
+      IBT_BASE.Rollback;
+    ibd_BASE.Close;
+  except
+    ibd_BASE.Free;
+  end;
+end;
+
+
+procedure TForm1.SeeClick(Sender: TObject);
+begin
+  p_OpenFileOrDirectory(DirectoryDest.Directory);
+end;
+
+procedure TForm1.AutoCreeIni(const as_FileName : String ; const as_Name: String);
+var lPackage : TPackageType;
+    lar_Architecture : TArchitectureType;
+    lProcType : TProcessorType ;
+begin
+  if pos ( 'Win', as_Name ) > 0 Then
+   lPackage:=ptExe;
+  if pos ( 'Deb', as_Name ) > 0 Then
+   lPackage:=ptDeb;
+  if pos ( 'MAC', as_Name ) > 0 Then
+   lPackage:=ptDmg;
+  if pos ( 'Redhat', as_Name ) > 0 Then
+   lPackage:=ptRpm;
+  if pos ( 'Tar', as_Name ) > 0 Then
+   lPackage:=ptTar;
+  if pos ( 'i386', as_Name ) > 0 Then
+   Begin
+    lProcType := ptIntel;
+    lar_Architecture:=at32;
+   end;
+  if pos ( 'i64', as_Name ) > 0 Then
+   Begin
+    lProcType := ptIntel;
+    lar_Architecture:=at64;
+   end;
+  if pos ( 'ARM', as_Name ) > 0 Then
+   Begin
+    lProcType := ptARM;
+    lar_Architecture:=at32;
+   end;
+  if pos ( 'PPC', as_Name ) > 0 Then
+   Begin
+    lProcType := ptPPC;
+    lar_Architecture:=at32;
+   end;
+  CreeIni ( as_FileName, lPackage, lar_Architecture, lProcType );
+end;
+function TForm1.CreeContext:Boolean;
+var Admin : Boolean;
+begin
+  Admin := True;
+  if FileExistsUTF8(FileBase.FileName)
+  and not ibd_BASE.Connected Then
+    ConnexionBase(ibd_BASE,FileBase.FileName,IBT_BASE,_lc_ctype,_user_name,_password,_role_name,'T_VERSION_BASE',Admin,Self);
+  if not DirectoryExistsUTF8(DirectoryExe.Directory)
+  or not DirectoryExistsUTF8 ( DirectoryDest.Directory )
+  or not FileExists(FileBase.FileName)
+   then
+    Begin
+     MessageDlg('Bottom panel not filled.', 'Please fill bottom panel.',mtError,[mbOK],0);
+     Result:=False;
+     Exit;
+    end;
+  Result:=True;
+end;
+
+procedure TForm1.CreeIni(const as_FileName : String ; const aPackage: TPackageType;
+  const aar_Architecture: TArchitectureType; const aProcType : TProcessorType );
+var Ainifile : TIniFile;
+const CST_INI_UPDATE_SECTION = 'UPDATE';
+begin
+ Ainifile := TIniFile.Create(DirectoryExe.Directory+DirectorySeparator+'Ancestromania.ini');
+ with Ainifile do
+ try
+  IniVersionExe(Ainifile);
+  InitUpdate(aar_Architecture,aPackage,aProcType);
+  if DirectoryExistsUTF8 ( DirectoryDest.Directory ) Then
+   Begin
+    Ainifile.Free;
+    Ainifile:=TIniFile.Create(DirectoryDest.Directory+DirectorySeparator+IniFilename+ExtensionIni);
+   end;
+  Ainifile.WriteString(CST_INI_UPDATE_SECTION,'Nom',ExtractFileName(as_FileName));
+  Ainifile.WriteString(CST_INI_UPDATE_SECTION,'Version','2013.0');
+  Ainifile.WriteString(CST_INI_UPDATE_SECTION,'VersionExe',NumVersionCourt(9,AncestroManiaVersion));
+  Ainifile.WriteString(CST_INI_UPDATE_SECTION,'Taille',IntToStr(FileSize(as_FileName)));
+  Ainifile.WriteString(CST_INI_UPDATE_SECTION,'Date',DateTimeToStr(Now));
+  Ainifile.WriteString(CST_INI_UPDATE_SECTION,'TailleMAJC',IntToStr(FileSize(as_FileName)));
+  Ainifile.WriteString(CST_INI_UPDATE_SECTION,'md5',MD5Print(MD5File(as_FileName)));
+  if ibd_BASE.Connected Then
+    Ainifile.WriteString(CST_INI_UPDATE_SECTION,'VersionBase',fs_GetVersionBase(q));
+
+ finally
+   Ainifile.Free;
+ end;
+end;
+
+
+initialization
+  OnGetLibraryName:=TOnGetLibraryName(p_setLibrary);
+
+end.
