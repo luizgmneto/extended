9c71388d685592cf312320b8d25612d34db36abd
girouxmatthieu <girouxmatthieu@localhost>
2014-01-08 12:32:07 +0100

    compiling on windows lazarus

--- lazextinit.pas
+++ lazextinit.pas
@@ -9,12 +9,13 @@ interface
 uses
   u_extmenucustomize, U_OnFormInfoIni, u_extmenutoolbar, U_FormAdapt, 
   U_FormMainIni, fonctions_forms, U_CustomizeMenu, fonctions_init, 
-  fonctions_vtree, LazarusPackageIntf;
+  fonctions_vtree, menutbar, LazarusPackageIntf;
 
 implementation
 
 procedure Register;
 begin
+  RegisterUnit('menutbar', @menutbar.Register);
 end;
 
 initialization
