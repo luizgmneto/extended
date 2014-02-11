1aa7aee60c31ff0e38fc3fbf28512c9027b92f1f
girouxmatthieu <girouxmatthieu@localhost>
2014-02-11 15:44:38 +0100

    creating linux packages

--- demos/CreateNetUpdateini/creedebrpm386.sh
+++ demos/CreateNetUpdateini/creedebrpm386.sh
@@ -4,17 +4,19 @@
 directory=`dirname $0`
 echo "Working in $directory"
 cd $directory 
-./version.sh ./CreateUpdateIni/DEBIAN/control ./i386-linux/CreateUpdateIni.ini
-cp ./i386-linux/CreateUpdateIni ./CreateUpdateIni/usr/share/createupdateini
-cp ./i386-linux/CreateUpdateIni.ini ./CreateUpdateIni/usr/share/createupdateini
-sudo chown -R root CreateUpdateIni.tar
-sudo tar -czvf CreateUpdateIni.tar.gz CreateUpdateIni.tar
-sudo chown -R matthieu CreateUpdateIni.tar
-sudo chown -R root CreateUpdateIni
-sudo dpkg-deb -b CreateUpdateIni 
-sudo chown -R matthieu CreateUpdateIni
+./version.sh ./CreateUpdateIni-gnome-i386/DEBIAN/control CreateUpdateIni.ini
+cp CreateUpdateIni CreateUpdateIni-gnome-i386/usr/share/createupdateini
+cp CreateUpdateIni.ini CreateUpdateIni-gnome-i386/usr/share/createupdateini
+cp CreateUpdateIni CreateUpdateIni-gnome-i386.tar
+cp CreateUpdateIni.ini CreateUpdateIni-gnome-i386.tar
+sudo chown -R root CreateUpdateIni-gnome-i386.tar
+sudo tar -czvf CreateUpdateIni-gnome-i386.tar.gz CreateUpdateIni-gnome-i386.tar
+sudo chown -R matthieu CreateUpdateIni-gnome-i386.tar
+sudo chown -R root CreateUpdateIni-gnome-i386
+sudo dpkg-deb -b CreateUpdateIni-gnome-i386
+sudo chown -R matthieu CreateUpdateIni-gnome-i386
 sudo chown matthieu *.deb
 rm *el.rpm
-sudo alien -r --scripts CreateUpdateIni.deb
+sudo alien -r --scripts CreateUpdateIni-gnome-i386.deb
 sudo chown matthieu *.rpm
-mv *i386*.rpm CreateUpdateIni.rpm
+mv *i386*.rpm CreateUpdateIni-gnome-i386.rpm
