13208b7c1577f21c6fecd8ba0dd8f413d704e319
girouxmatthieu <girouxmatthieu@localhost>
2014-07-29 09:59:21 +0200

    testing

--- extendsinc.sh
+++ extendsinc.sh
@@ -4,7 +4,13 @@
 directory=`dirname $0`
 echo "Working in $directory"
 cd $directory
-cp ./*.inc ../Ancestromania/Packages/AncestroComponents/
-cp ./*.inc ../ManFrames/
-cp ./*.inc ../XMLFrames/
+rm ../Ancestromania/Packages/AncestroComponents/*.inc
+ln -s $directory/extends.inc ../Ancestromania/Packages/AncestroComponents/extends.inc
+ln -s $directory/dlcompilers.inc ../Ancestromania/Packages/AncestroComponents/dlcompilers.inc
+rm ../ManFrames/*.inc
+ln -s $directory/extends.inc ../ManFrames/extends.inc
+ln -s $directory/dlcompilers.inc ../ManFrames/dlcompilers.inc
+rm ../XMLFrames/*.inc
+ln -s $directory/extends.inc ../XMLFrames/extends.inc
+ln -s $directory/dlcompilers.inc ../XMLFrames/dlcompilers.inc
 
