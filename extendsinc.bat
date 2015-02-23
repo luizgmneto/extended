40afb3d51be124a55577b47c30977f8a37077478
girouxmatthieu <girouxmatthieu@localhost>
2015-02-23 10:36:45 +0100

    linux compatible

--- extendsinc.bat
+++ extendsinc.bat
@@ -4,6 +4,10 @@ cd %CD%
 
 cd ..
 
+del Ancestromania\Packages\AncestroComponents\*.inc
+del ManFrames\*.inc
+del XMLFrames\*.inc
+
 copy Extended\*.inc Ancestromania\Packages\AncestroComponents\
 copy Extended\*.inc ManFrames\
 copy Extended\*.inc XMLFrames\
